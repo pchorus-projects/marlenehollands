180d1c1808411477469660ec2642a2e668a2d566
Pascal Chorus <p.chorus@me.com>
2021-07-15 19:45:42 +0200

    refactor(upcoming-dates): move upcoming-dates to a component

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -27,60 +27,17 @@
 
     <section class="section section--alternate">
       <h2 class="heading heading--xxl heading--xxl-paragraph u-max-page-width">Aktuelle Termine</h2>
-      <ul class="upcoming-dates u-max-page-width">
-        <li
-          class="upcoming-dates__item"
-          v-for="upcomingDate in this.$page.prismic.upcomingDates.edges"
-          :key="upcomingDate.title"
-        >
-          <p class="heading heading--l u-margin-bottom-s">{{ upcomingDate.node.title[0].text }}</p>
-          <p class="u-font-s u-margin-bottom-s">
-            {{ new Date(upcomingDate.node.start).toLocaleDateString() }} -
-            {{ new Date(upcomingDate.node.end).toLocaleDateString() }}
-          </p>
-          <p class="u-font-m" v-html="upcomingDate.node.address[0].text.replace(/\n/g, '<br />')"></p>
-          <a
-            v-if="upcomingDate.node.link"
-            style="display: block"
-            class="link u-margin-top-s"
-            :href="upcomingDate.node.link.url"
-            target="_blank"
-            rel="noopener"
-            >Zur Webseite</a
-          >
-        </li>
-      </ul>
+      <upcoming-dates class="u-max-page-width"></upcoming-dates>
     </section>
   </Layout>
 </template>
 
-<page-query>
-query UpcomingDates {
-  prismic {
-    upcomingDates: allUpcoming_dates(sortBy: start_ASC) {
-      edges {
-        node {
-          title
-          start
-          end
-          address
-          link {
-            ... on prismic__ExternalLink {
-              url
-            }
-          }
-        }
-      }
-    }
-  }
-}
-</page-query>
-
 <script>
 import TextAndImage from '../components/TextAndImage';
+import UpcomingDates from '../components/UpcomingDates';
 
 export default {
-  components: { TextAndImage },
+  components: { TextAndImage, UpcomingDates },
   metaInfo: {
     title: 'Marlene Hollands',
   },
@@ -181,23 +138,6 @@ export default {
   }
 }
 
-.upcoming-dates {
-  display: flex;
-  justify-content: space-between;
-
-  &__item {
-    flex: 1 0 0;
-    background-color: #ffffff;
-    padding: $space-m;
-    box-shadow: 0 0 50px -22px $shadow-default-color;
-    border-radius: $border-radius-default;
-  }
-
-  > :not(:last-child) {
-    margin-right: $space-xl;
-  }
-}
-
 @media (max-width: $max-width-tablet) {
   .hero {
     &__text {
@@ -212,14 +152,6 @@ export default {
 
     &__subtitle {
       display: none;
-    }
-  }
-
-  .upcoming-dates {
-    display: block;
-
-    > :not(:last-child) {
-      margin: 0 0 $space-l 0;
     }
   }
 }
